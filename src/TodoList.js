dd449ba849864ae1c3095dde8f9be1b92623c52f
Linus Niedermaier <Linus.Niedermaier@citrix.com>
2017-03-13 16:11:11 +0100

    some css changes

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,6 +6,7 @@ class InputField extends Component {
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleKeyPressed = this.handleKeyPressed.bind(this);
   }
   handleInputChange(event) {
     this.props.onInputChange(event.target.value);
@@ -13,16 +14,35 @@ class InputField extends Component {
   handleButtonClick(event) {
     this.props.onClick(event);
   }
+  handleKeyPressed(event) {
+    if (event.key === 'Enter') {
+      this.props.onClick(event);
+    }
+  }
   render() {
-    return <div><input value={this.props.inputValue} onChange={this.handleInputChange} type="text" placeholder="Oh i need to remember ..." /><button onClick={this.handleButtonClick} >Add</button> </div>;
+    return <div className="searchBar"><input className="searchBarItem" value={this.props.inputValue} onKeyPress={this.handleKeyPressed} onChange={this.handleInputChange} type="text" placeholder="Oh i need to remember ..." /><button className="searchBarItem" onClick={this.handleButtonClick} >Add</button> </div>;
   }
 }
 
-function FilterButtons(props) {
+const FilterButtons = function(props) {
   // handleButtonClick(event.target.value){
   //   props.onButtonClick(event.target.value);
   // }
-  return <div><button onClick={() => { props.onButtonClick('all') }} >All</button> <button onClick={() => { props.onButtonClick('done') }} >Done</button> <button onClick={() => { props.onButtonClick('incomplete') }} >Incomplete</button></div>
+  let buttonValues = ["all", "done", "incomplete"];
+  let buttons = buttonValues.map((buttonValue, index) => {
+    let buttonCn = "filterButton";
+    console.log("FilterButtons [props.filterTerm, buttonValue]", props.filterTerm, buttonValue);
+    if(props.filterTerm === buttonValue){
+    console.log("FilterButtons [props.filterTerm, buttonValue] true", props.filterTerm, buttonValue);
+      buttonCn =  buttonCn + " selected";
+    }
+    return <button className={buttonCn} key={index} onClick={() => { props.onButtonClick(buttonValue) }} >{buttonValue}</button>  
+  });
+  return <div className="filterButtons">{buttons}</div>
+}
+
+FilterButtons.propTypes ={
+  filterTerm: React.PropTypes.oneOf(['all','done', 'incomplete'])
 }
 
 class TodoList extends Component {
@@ -31,8 +51,8 @@ class TodoList extends Component {
     this.state = {
       todos: [],
       inputText: "",
-      completeOnly: false,
-      incompleteOnly: false
+      // filterTerm can be 'all', 'done', 'incomplete'
+      filterTerm: "all"
     }
 
     this.handleTodoAdd = this.handleTodoAdd.bind(this);
@@ -41,6 +61,7 @@ class TodoList extends Component {
     this.handleCheckboxClick = this.handleCheckboxClick.bind(this);
   }
   handleTodoAdd() {
+    console.log("handleTodoAdd");
     let todos = this.state.todos;
     todos.push({ displayText: this.state.inputText, done: false });
     this.setState({ todos: todos, inputText: "" });
@@ -49,18 +70,7 @@ class TodoList extends Component {
     this.setState({ inputText: inputValue });
   }
   handleFilterButtonClick(filterValue) {
-    switch (filterValue) {
-      case 'done':
-        this.setState({ completeOnly: true, incompleteOnly: false });
-        break;
-      case 'incomplete':
-        this.setState({ completeOnly: false, incompleteOnly: true });
-        break;
-      // 'all'
-      default:
-        this.setState({ completeOnly: false, incompleteOnly: false });
-        break;
-    }
+    this.setState({ filterTerm: filterValue });
   }
   handleCheckboxClick(elementClicked) {
     let todos = this.state.todos;
@@ -73,23 +83,23 @@ class TodoList extends Component {
   }
   render() {
     let todos = this.state.todos.map((element, index) => {
-      if ((!this.state.completeOnly && !this.state.incompleteOnly) || (this.state.completeOnly && element.done) || (this.state.incompleteOnly && !element.done)) {
-        return <li key={index}><input type="checkbox" value={element.done} onClick={() => { this.handleCheckboxClick(element) }} /> {element.displayText}</li>
+      if (this.state.filterTerm === 'all' || (this.state.filterTerm === 'done' && element.done) || (this.state.filterTerm === 'incomplete' && !element.done)) {
+        return <li key={index} className="todoUnsortedListItem"><input type="checkbox" checked={element.done} onChange={() => { this.handleCheckboxClick(element) }} /> {element.displayText}</li>
       }
       else {
         return null;
       }
     });
     return (
-      <div>
-        <div>
-          <h2>Todo-List</h2>
+      <div className="todolist">
+        <div className="todolistTitleDiv">
+          <h2 className="todolistTitle">Todo-List</h2>
         </div>
         <InputField onClick={this.handleTodoAdd} inputValue={this.state.inputText} onInputChange={this.handleInputChange} />
-        <ul style={{ listStyle: 'none' }}>
+        <FilterButtons onButtonClick={this.handleFilterButtonClick} filterTerm={this.state.filterTerm} />
+        <ul className="todoUnsortedList" style={{ listStyle: 'none' }}>
           {todos}
         </ul>
-        <FilterButtons onButtonClick={this.handleFilterButtonClick} />
       </div>
     );
   }
